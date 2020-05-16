b5b4deda8fefaa295e32efcbaecb8410b2d92b9a
Oleksii Kupin <interstates21@gmail.com>
2020-05-16 22:16:15 +0300

    load pdf on client

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,15 +3,17 @@ import { Link } from "gatsby";
 
 import Layout from "../components/app";
 import Timeline from "../components/Timeline";
-import CV from "../components/CV";
 import SEO from "../components/seo";
 import Skills from "../components/Skills";
+import Loadable from "@loadable/component";
 import { Typography } from "@material-ui/core";
+
+const LoadableCV = Loadable(() => import("../components/CV"));
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <CV />
+    <LoadableCV />
     <Skills />
     <Typography
       variant="h2"
